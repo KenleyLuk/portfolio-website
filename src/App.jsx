e94d9e0c3df74c0c0863997bdeedb4a5622c6f62
KenleyLuk <kentyhung@gmail.com>
2024-05-08 13:44:48 -0400

    feat: create experience page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import styles from "./App.module.css";
 import { About } from "./componets/About/About";
 import { Hero } from "./componets/Hero/Hero";
 import { Navbar } from "./componets/Navbar/Navbar";
+import { Experience } from "./componets/Experience/Experience";
 
 function App() {
   return <div className={styles.App}>
     <Navbar />
     <Hero />
     <About />
+    <Experience />
   </div>
 }
 
