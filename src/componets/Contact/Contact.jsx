445856c0188865a9c64c7da8b5cddf3681bd0a07
KenleyLuk <kentyhung@gmail.com>
2024-05-11 12:24:01 -0400

    refactor: update configue

--- src/componets/Contact/Contact.jsx
+++ src/componets/Contact/Contact.jsx
@@ -20,7 +20,7 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://www.linkedin.com/in/kenley-luk-14a573181/">linkedin.com/Kenley</a>
+          <a href="https://www.linkedin.com/in/kenley-luk-14a573181/">linkedin.com/KenleyLuk</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
